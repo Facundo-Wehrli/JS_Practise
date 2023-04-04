4343124daf45080022bf7592f160bb3c037258ec
Facundo <wehrlifacundo9@gmail.com>
2023-04-04 19:39:30 -0300

    exercise finished

---
+++ print_current_window_contents/js.js
@@ -0,0 +1,8 @@
+/*EXP window.print():
+ The window object represents a window containing a DOM document;
+ the document property points to the DOM document loaded in that window,
+ window.print() is used to open the Print Dialog to print the current document.  */
+
+function print_current_page() {
+    window.print();
+}
