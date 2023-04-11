9b3d169e5f9625d18c2397aed30d03ab70232b6d
Facundo <wehrlifacundo9@gmail.com>
2023-04-11 20:17:03 -0300

    passenger counter done

--- passenger_counter/index.js
+++ passenger_counter/index.js
@@ -1,12 +1,22 @@
+//1. grab the save-el paragraph and store it in a variable called saveEl
+let Elcount = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+let count = 0;
+
 function increment() {
-
+    count += 1;
+    Elcount.textContent = count;
 }
-function incrementLapsCompleted() {
-    let lapsCompleted = 0;
-    lapsCompleted = lapsCompleted + 1;
-}
-incrementLapsCompleted();
-incrementLapsCompleted();
-incrementLapsCompleted();
 
-console.log(lapsCompleted);
+//2 create a variable that contains both the count and the dash separator, i.e "12-"
+//3. render teh variable in the save-el using innetText
+//TODO make sure to not delete the existing content of the paragraph
+function save() {
+    let entries = count + " - ";
+    saveEl.textContent += entries;
+    //reset the counter to 0 every time you press save
+    Elcount.textContent = 0;
+    count = 0;
+}
+
+
