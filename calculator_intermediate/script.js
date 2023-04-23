dbd39bef545bf92de86989b263f5fabe9ba6137a
Facundo <wehrlifacundo9@gmail.com>
2023-04-22 23:46:58 -0300

    calculator js progress

--- calculator_intermediate/script.js
+++ calculator_intermediate/script.js
@@ -1,19 +1,51 @@
+/* This code is selecting HTML elements with the IDs "btn0" through "btn9" and assigning them to
+corresponding constants named btn0 through btn9. These constants can then be used in JavaScript code
+to manipulate or interact with those HTML elements. */
 const btn0 = document.getElementById("btn0");
-btn0.addEventListener("click", function () {
-    let output = output.getElementById("output")
-    /* assigning the current text content of the HTML
-    element with the ID "display" to the variable `displayValue`. This allows the current value
-    displayed on the screen to be stored in a variable and manipulated later. */
-    let outputValue = output.innerText;
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+const btn3 = document.getElementById("btn3");
+const btn4 = document.getElementById("btn4");
+const btn5 = document.getElementById("btn5");
+const btn6 = document.getElementById("btn6");
+const btn7 = document.getElementById("btn7");
+const btn8 = document.getElementById("btn8");
+const btn9 = document.getElementById("btn9");
 
-    /* appending the number 0 to the current value of `displayValue`. This is
-    equivalent to `displayValue = displayValue + 0;`. In this case, it is used to add the number 0
-    to the current value displayed on the screen when the button with the ID "btn0" is clicked. */
-    outputValue += "0";
+const bntAdd = document.getElementById("btn-add");
+const btnSubstract = document.getElementById("btn-substract");
+const btnDecimal = document.getElementById("btn-decimal");
+const btnDivide = document.getElementById("btn-divide");
+const btnClear = document.getElementById("btn-clear");
+const btnEquals = document.getElementById("btn-equals");
+const btnBackspace = document.getElementById("btn-backspace");
+const display = document.querySelector(".display");
 
-    /* setting the text content of the `display` element to the
-    value of `displayValue`. This means that when the button with the ID "btn0" is clicked, the
-    number 0 is added to the current value displayed on the screen. */
-    output.innerText = outputValue;
+let currentNumber;
 
-})
+btn0.addEventListener("click", () => {
+    currentNumber = 0;
+    display.textContent = currentNumber;
+});
+btn1.addEventListener("click", () => { });
+btn2.addEventListener("click", () => { });
+btn3.addEventListener("click", () => { });
+btn4.addEventListener("click", () => { });
+btn5.addEventListener("click", () => { });
+btn6.addEventListener("click", () => { });
+btn7.addEventListener("click", () => { });
+btn8.addEventListener("click", () => { });
+btn9.addEventListener("click", () => { });
+display.addEventListener("click", () => { });
+
+
+bntAdd.addEventListener("click", () => { })
+btnSubstract.addEventListener("click", () => { })
+btnDecimal.addEventListener("click", () => { })
+btnDivide.addEventListener("click", () => { })
+btnClear.addEventListener("click", () => { })
+btnEquals.addEventListener("click", () => { })
+btnBackspace.addEventListener("click", () => { })
+
+
+
